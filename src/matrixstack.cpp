51e9e5d912647fcf88f74cf94efff0ce8ceb81c3
Xin <xinwen@mit.edu>
2018-10-17 23:05:45 -0400

    added src code for pset2 hierarchial modeling and ssd

---
+++ src/matrixstack.cpp
@@ -0,0 +1,38 @@
+#include "matrixstack.h"
+using namespace std;
+
+MatrixStack::MatrixStack()
+{
+	// Initialize the matrix stack with the identity matrix.
+    m_matrices.push_back(Matrix4f::identity());
+}
+
+void MatrixStack::clear()
+{
+	// Revert to just containing the identity matrix.
+    m_matrices.clear();
+    m_matrices.push_back(Matrix4f::identity());
+}
+
+Matrix4f MatrixStack::top()
+{
+	// Return the top of the stack
+	// return Matrix4f();
+    
+    return m_matrices.back();
+}
+
+void MatrixStack::push( const Matrix4f& m )
+{
+	// Push m onto the stack.
+	// The new top should be "old * m", so that conceptually the new matrix
+    // is applied first in right-to-left evaluation.
+    Matrix4f oldTop = m_matrices.back();
+    m_matrices.push_back(oldTop * m);
+}
+
+void MatrixStack::pop()
+{
+	// Remove the top element from the stack
+    m_matrices.pop_back();
+}
